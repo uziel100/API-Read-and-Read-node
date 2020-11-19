20d616745f4b094ae81f6e691d128e31afa1318e
Uziel <uzielmelitonh@gmail.com>
2020-11-19 00:02:29 -0600

    Add new fields for model

--- server/models/User.js
+++ server/models/User.js
@@ -1,41 +1,69 @@
-const mongoose = require('mongoose');
-const unique = require('mongoose-unique-validator')
-
+const mongoose = require("mongoose");
+const unique = require("mongoose-unique-validator");
+const Enviroment = require("../config/enviroment");
 
 let Schema = mongoose.Schema;
+const rolesValids = {
+  values: Enviroment.roles,
+  message: "{VALUE} no es un rol valido",
+};
 
 let userSchema = new Schema({
-    name: {
-        type: String,
-        required: [false, "El nombre es obligatorio"]
-    },
-    email:{
-        type: String,
-        unique: true,
-        required: [true, "El email es obligatorio"]
-    },
-    password: {
-        type: String,
-        required: [true, "La contraseña es obligatorio"]
-    },
-    address: {
-        type: String,
-        required: [false, "La dirección es obligatorio"]
-    },
-    phone: {
-        type: String,        
-        required: [false, "El telefono es obligatorio"]
-    },
-    birthDate: {
-        type: String,
-        required: [false, "La fecha de nacimiento es obligatorio"]
-    },
-    gender: {
-        type: String,
-        required: [false, "El sexo es obligatorio"]
-    }        
-})
+  name: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "El email es obligatorio"],
+  },
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatorio"],
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  phone: {
+    type: String,
+    required: false,
+  },
+  birthDate: {
+    type: String,
+    required: false,
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    default: "USER_ROLE",
+    enum: rolesValids,
+  },
+  signWithGoogle: {
+    type: Boolean,
+    default: false,
+  },
+});
 
+userSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+  delete userObject.password;
 
-userSchema.plugin( unique, { message: '{PATH} debe ser unico' } );
-module.exports = mongoose.model('User', userSchema );
+  return userObject;
+};
+
+userSchema.plugin(unique, { message: "{PATH} debe ser unico" });
+module.exports = mongoose.model("User", userSchema);
